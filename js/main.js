83ac844d241bdc6ed883bb72cf6e563044f28945
Константин Музыка <86382755+Kosatos@users.noreply.github.com>
2021-08-26 22:55:28 +0300

    last ver

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
     let floorPath = $('.home-image path'); //каждый отдельный этаж в SVG
     let counterUp = $('.counter-up');  //кнопка увеличения этажа
     let counterDown = $('.counter-down');  //кнопка уменьшения этажа
+    let buttonPrimary = $('.button-primary');
+    let modal = $('.modal');
+    let closeButton = $('.modal-close-button');
+    let flatsItem = $('.flats-item');
+    let flatsPath = $('.flats path')
 
     //функция при наведении мышки на этаж
     floorPath.on('mouseover', function() {  
@@ -10,6 +15,16 @@ $(document).ready(function () {
         currentFloor = $(this).attr('data-floor');  //получаем значение текущего этажа
         $('.counter').text(currentFloor);  //записываем значение этажа в счетчик
     })
+
+    function openCloseModal() {
+        modal.toggleClass('open-modal');
+    }
+
+    floorPath.on('click', openCloseModal);
+
+    buttonPrimary.on('click', openCloseModal);
+
+    closeButton.on('click', openCloseModal);
 
     //отслеживаем клик по кнопке ВВЕРХ
     counterUp.on('click', function() {
@@ -30,5 +45,9 @@ $(document).ready(function () {
             floorPath.removeClass('current-floor');
             $(`[data-floor = ${usCurrrentFloor}]`).toggleClass('current-floor');
         }
-    })    
+    }) 
+    
+    // flatsItem.on('mouseover', function() {  
+    //     flatsPath.toggleClass('current-flat');  //удаляем активный класс у этажей
+    // })
 });
